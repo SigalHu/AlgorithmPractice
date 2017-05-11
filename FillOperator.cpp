a35722ac4ee1c3e84e5f5e7fc1eed99502f1e503
SigalHu <811734597@qq.com>
2017-05-11 22:51:40 +0800

    上传代码

---
+++ FillOperator.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <array>
+#include <sstream>
+#include <regex>
+
+using namespace std;
+
+bool getResult(string eval,double *result){
+    stringstream ss;
+    double dbl;
+    smatch resm;
+    regex rep("\\+\\d+(\\.\\d+)?");
+    regex_search(eval,resm,rep);
+    ss<<resm[0];
+    ss>>*result;
+    ss.clear();
+    eval = resm.suffix().str();
+
+    while(regex_search(eval,resm,rep)){
+        ss<<resm[0];
+        ss>>dbl;
+        ss.clear();
+
+        if(eval[0] == '*')
+            *result *= dbl;
+        else{
+            if(!dbl)
+                return false;
+            *result /= dbl;
+        }
+        eval = resm.suffix().str();
+    }
+    return true;
+}
+
+int main() {
+    array<double,6> arrDataIn;
+    array<string,4> arrOperator={"+","-","*","/"};
+
+    cout << "ÇëÊäÈë6¸öÊý×Ö£º";
+    for(double &data:arrDataIn){
+        cin>>data;
+    }
+
+    string strData,strTmp;
+    stringstream ss;
+    regex rep("[+-]\\d+(\\.\\d+)?([*/]\\d+(\\.\\d+)?)*");
+    smatch resm;
+    double dblTmp,dblRes;
+    bool isSucceed;
+    for(string operator1:arrOperator){
+        for(string operator2:arrOperator){
+            for(string operator3:arrOperator){
+                for(string operator4:arrOperator){
+                    dblRes = 0;
+                    isSucceed = true;
+
+                    ss<<"+"<<arrDataIn[0]<<operator1<<arrDataIn[1]<<operator2
+                      <<arrDataIn[2]<<operator3<<arrDataIn[3]<<operator4<<arrDataIn[4];
+                    ss>>strData;
+                    ss.clear();
+
+                    strTmp = strData;
+                    while(regex_search(strTmp,resm,rep)){
+                        isSucceed = getResult(resm[0].str(),&dblTmp);
+                        if(isSucceed)
+                            dblRes += dblTmp;
+                        else
+                            break;
+                        strTmp = resm.suffix().str();
+                    }
+
+                    if(isSucceed && (dblRes>arrDataIn[5]?(dblRes-arrDataIn[5]):(arrDataIn[5]-dblRes)) < 1e-5){
+                        cout<<strData.substr(1)<<"="<<arrDataIn[5]<<endl;
+                    }
+                }
+            }
+        }
+    }
+
+//    cin.get();
+    return 0;
+}
